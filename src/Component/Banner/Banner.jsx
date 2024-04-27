ecd639cf1fda13139ab518ffd396abe1902ff47d
Shahriyar <chyshahriyar@gmail.com>
2024-04-28 00:16:23 +0600

    Banner and error page complete and responsive

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -28,10 +28,10 @@ const Banner = () => {
                 <SwiperSlide>
                     <div className=' bg-cover bg-center min-h-screen' style={{ backgroundImage: "url('https://i.ibb.co/WPHyFrZ/slider0.jpg')" }}>
                         <div className='bg-black min-h-screen bg-opacity-60'>
-                            <div className='pt-44'>
-                                <h2 className='text-[#FFFFFF] text-center w-4/6 mx-auto text-6xl font-bold'>Take your creativity anywhere with the <span className='text-[#A1662F]'>Prakritik Shongi..</span></h2>
-                                <p className='text-[#FFFFFF] text-center w-9/12 mx-auto text-xl font-semibold mt-5'>Discover exquisite handcrafted jute and wood creations, blending natural elements with artisanal expertise for timeless elegance in home decor.</p>
-                                <div className='w-6/12 mx-auto  mt-10'>
+                            <div className='lg:pt-44 pt-36'>
+                                <h2 className='text-[#FFFFFF] text-center w-5/6 lg:w-4/6 mx-auto text-3xl lg:text-6xl font-bold'>Take your creativity anywhere with the <span className='text-[#A1662F]'>Prakritik Shongi..</span></h2>
+                                <p className='text-[#FFFFFF] text-center lg:w-9/12 w-10/12 mx-auto text-base lg:text-xl font-semibold mt-5'>Discover exquisite handcrafted jute and wood creations, blending natural elements with artisanal expertise for timeless elegance in home decor.</p>
+                                <div className='lg:w-6/12 w-4/5 mx-auto mt-10'>
                                     <label className="input rounded-full flex items-center gap-2 h-12 ">
                                         <input type="text" className="grow rounded-full" placeholder="   Search here" />
                                         <button className="h-10 bg-[#bdac8f] text-[#FFFFFF] rounded-full w-10 border-[#bdac8f] text-xl"><IoSearch className="text-xl ml-2" /></button>
@@ -45,9 +45,9 @@ const Banner = () => {
                     <div className=' bg-cover bg-center min-h-screen' style={{ backgroundImage: "url('https://i.ibb.co/rsf6zVL/slider1.jpg')" }}>
                         <div className='bg-black min-h-screen bg-opacity-60'>
                             <div className='pt-48'>
-                                <h2 className='text-[#FFFFFF] text-center w-4/6 mx-auto text-6xl font-bold'>Do not just dream it. <span className='text-[#8b4513]'>Make it..</span></h2>
-                                <p className='text-[#FFFFFF] text-center w-9/12 mx-auto text-xl font-semibold mt-9'>Transform your aspirations into reality with our exquisite jute and wood crafts, embodying creativity, sustainability, and timeless elegance.</p>
-                                <div className='w-6/12 mx-auto  mt-10'>
+                                <h2 className='text-[#FFFFFF] text-center w-5/6 lg:w-4/6 mx-auto text-3xl lg:text-6xl font-bold'>Do not just dream it. <span className='text-[#8b4513]'>Make it..</span></h2>
+                                <p className='text-[#FFFFFF] text-center lg:w-9/12 w-10/12 mx-auto text-base lg:text-xl font-semibold mt-9'>Transform your aspirations into reality with our exquisite jute and wood crafts, embodying creativity, sustainability, and timeless elegance.</p>
+                                <div className='lg:w-6/12 w-4/5 mx-auto  mt-10'>
                                     <label className="input rounded-full flex items-center gap-2 h-12 ">
                                         <input type="text" className="grow rounded-full" placeholder="   Search here" />
                                         <button className="h-10 bg-[#bdac8f] text-[#FFFFFF] rounded-full w-10 border-[#bdac8f] text-xl"><IoSearch className="text-xl ml-2" /></button>
@@ -61,9 +61,9 @@ const Banner = () => {
                     <div className=' bg-cover bg-center min-h-screen' style={{ backgroundImage: "url('https://i.ibb.co/H2x1D8S/slider3.jpg')" }}>
                         <div className='bg-black min-h-screen bg-opacity-60'>
                             <div className='pt-44'>
-                                <h2 className='text-[#FFFFFF] text-center w-4/6 mx-auto text-6xl font-bold'>Turning Dreams into <span className='text-[#a0522d]'>Artisanal</span> Reality.</h2>
-                                <p className='text-[#FFFFFF] text-center w-9/12 mx-auto text-xl font-semibold mt-8'>From dreams to handcrafted wonders, witness the magic of jute and wood craftsmanship in our artisanal collection.</p>
-                                <div className='w-6/12 mx-auto  mt-10'>
+                                <h2 className='text-[#FFFFFF] text-center w-5/6 lg:w-4/6 mx-auto text-3xl lg:text-6xl font-bold'>Turning Dreams into <span className='text-[#a0522d]'>Artisanal</span> Reality.</h2>
+                                <p className='text-[#FFFFFF] text-center lg:w-9/12 w-10/12 mx-auto text-base lg:text-xl font-semibold mt-8'>From dreams to handcrafted wonders, witness the magic of jute and wood craftsmanship in our artisanal collection.</p>
+                                <div className='lg:w-6/12 w-4/5 mx-auto  mt-10'>
                                     <label className="input rounded-full flex items-center gap-2 h-12 ">
                                         <input type="text" className="grow rounded-full" placeholder="   Search here" />
                                         <button className="h-10 bg-[#bdac8f] text-[#FFFFFF] rounded-full w-10 border-[#bdac8f] text-xl"><IoSearch className="text-xl ml-2" /></button>
--- src/Page/Error/Error.jsx
+++ src/Page/Error/Error.jsx
@@ -1,13 +1,18 @@
-
+import { Helmet } from "react-helmet-async";
 
 const Error = () => {
     return (
-        <div className="relative">
-            <div className="absolute inset-0 bg-opacity-50 bg-black"></div>
-            <div className="background-image bg-cover bg-center h-screen">
-                <div className="py-60">
-                    <h2 className="text-xl mb-16 text-center">The page you are looking for can not be found.</h2>
-                    <h1 className="text-9xl font-extrabold text-center">404</h1>
+        <div>
+            <Helmet>
+                <title>prakritik-shongi-error</title>
+            </Helmet>
+            <div className="relative" style={{ backgroundImage: "url('https://i.ibb.co/BsMMCB6/Error.jpg')" }}>
+                <div className="absolute inset-0 "></div>
+                <div className="bg-cover bg-center h-screen">
+                    <div className="py-60 bg-opacity-70 bg-black">
+                        <h2 className="md:text-xl text-lg mb-16 text-center text-[#bdac8f] w-11/12 mx-auto">The page you are looking for can not be found.</h2>
+                        <h1 className="md:text-9xl text-6xl font-extrabold text-center text-[#8b4513]">404</h1>
+                    </div>
                 </div>
             </div>
         </div>
